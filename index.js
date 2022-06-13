7cda68a7b5cf585cdd45b8891d39adb393cbb498
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-13 15:23:03 +0300

    commit tree d891ef8c945f1fae141471c64b5dfd91112eb392

--- index.js
+++ index.js
@@ -1,44 +1,76 @@
 // Your code here
-const dodger = document.getElementById("dodger");
+const dodger = document.getElementById('dodger');
 dodger.style.backgroundColor = "#FF69B4";
 
-function moveDodgerLeft() {
-  const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
-  if (left > 0) {
-    dodger.style.left = `${left - 5}px`;
-  }
+//move dodger right
+const moveDodgerRight =() => {
+  const rightNumbers = dodger.style.left.replace('px','');
+  const right =parseInt(rightNumbers,10);
+  right<360? dodger.style.left =`${right + 5}px`: '';
 }
-function moveDodgerRight() {
-  const rightNumbers = dodger.style.left.replace("px", "");
-  const right = parseInt(rightNumbers, 10);
-  if (right < 360) {
-    dodger.style.left = `${right + 5}px`;
-  }
+//move dodger left
+const moveDodgerLeft =()=>{
+  const leftNumbers = dodger.style.left.replace('px','');
+  const left = parseInt(leftNumbers,10);
+  left>0? dodger.style.left= `${left - 5 }px`: '';
 }
-function moveDodgerUp() {
-  const topNumbers = dodger.style.bottom.replace("px", "");
-  const top = parseInt(topNumbers, 10);
-  if (top < 360) {
-    dodger.style.bottom = `${top + 5}px`;
-  }
+//move dodger down
+const moveDodgerDown =()=>{
+  const downNumbers = dodger.style.top.replace('px','');
+  const down = parseInt(downNumbers,10);
+  top<360? dodger.style.top= `${down + 5 }px`: '';
 }
-function moveDodgerDown() {
-  const bottomNumbers = dodger.style.top.replace("px", "");
-  const down = parseInt(bottomNumbers, 10);
-  if (down < 360) {
-    dodger.style.top = `${down + 5}px`;
-  }
+//move dodger top
+const moveDodgerTop =()=>{
+  const topNumbers = dodger.style.top.replace('px','');
+  const top = parseInt(topNumbers,10);
+  top>0? dodger.style.top= `${top - 5 }px`: '';
 }
 
+
 document.addEventListener("keydown", function (event) {
-  if (event.key === "ArrowLeft") {
-    moveDodgerLeft();
-  } else if (event.key === "ArrowRight") {
-    moveDodgerRight();
-  } else if (event.key === "ArrowUp") {
-    moveDodgerUp();
-  } else if (event.key === "ArrowDown") {
-    moveDodgerDown();
-  }
-});
+  (event.key === "ArrowLeft")? moveDodgerLeft(): 
+  (event.key === "ArrowRight")? moveDodgerRight():
+  (event.key === "ArrowUp") ? moveDodgerUp(): 
+  (event.key === "ArrowDown")? moveDodgerDown():''
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
